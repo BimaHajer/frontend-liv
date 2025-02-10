ee1cdc1679be46a3645cc4a266f71590a8fe58be
dhouhahmd <dhouhahamoudi@gmail.com>
2025-02-10 15:34:01 +0100

    ajoute des modifications

--- font-end-livraison/src/app/app.module.ts
+++ font-end-livraison/src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import '@cds/core/icon/register.js';
+import { ClarityIcons, userIcon } from '@cds/core/icon';
 
+ClarityIcons.addIcons(userIcon);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,ClarityModule
+    AppRoutingModule,ClarityModule,CoreModule,BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
